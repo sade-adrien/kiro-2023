2887d56082e3742eb2018ae1b76a03c754454faa
Adrien <sade-adrien@live.fr>
2023-11-09 08:19:59 -0800

    add cluster

--- second_sol.py
+++ second_sol.py
@@ -1,4 +1,7 @@
 from tools import *
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
 
 def turbines_sol(data):
     turbines_sol = []
@@ -13,3 +16,16 @@ def substation_substation_cables_sol(data):
 def substations_sol(data):
     return [substation(1, 1, 1).to_dict()]
 
+def turbines_cluster(data, n_clusters):
+    idxy = pd.DataFrame().from_dict(data['wind_turbines'])
+    id = idxy['id'].to_numpy()
+    x = idxy[['x']].to_numpy()
+    y = idxy[['y']].to_numpy()
+
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(y)
+
+    cluster_centers = np.concatenate((np.mean(x) * np.ones_like(kmeans.cluster_centers_), kmeans.cluster_centers_), axis=1)
+    labels = kmeans.labels_
+    
+    return (cluster_centers, labels)
