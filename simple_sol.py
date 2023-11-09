9785e46aa2e52fd4105d5f2ef762946b70d276d7
Adrien <sade-adrien@live.fr>
2023-11-09 07:08:16 -0800

    add turbines sol

--- simple_sol.py
+++ simple_sol.py
@@ -1,6 +1,11 @@
 from tools import *
 
-def turbine
+def turbines_sol(data):
+    turbines_sol = []
+    for t in data['wind_turbines']:
+        turbines_sol.append(turbine(id=t['id'],
+                                    substation_id=1).to_dict())
+    return turbines_sol
 
 def substation_substation_cables_sol():
     return []
